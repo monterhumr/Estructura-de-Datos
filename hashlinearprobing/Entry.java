ca31090f8aac90cb7fe44aa6f64439d74a587544
monterhumr <monterhector@gmail.com>
2017-03-02 20:27:59 -0600

    Create Entry.java

---
+++ hashlinearprobing/Entry.java
@@ -0,0 +1,17 @@
+public class Entry {
+    private String key;
+    private int data;
+
+    Entry(String key, int data) {
+          this.key = key;
+          this.data = data;
+    }     
+
+    public String getKey() {
+          return key;
+    }
+
+    public int getData() {
+          return data;
+    }
+}
