242670ce45434965f85ef62d6fe2cbc37fae7cdd
monterhumr <monterhector@gmail.com>
2017-03-17 11:21:05 -0600

    Create Entry.java

---
+++ universalhashing/Entry.java
@@ -0,0 +1,20 @@
+public class Entry {
+    private String key;
+    private int data;
+
+
+    public Entry(String key, int data) {
+          this.key = key;
+          this.data = data;
+
+    }     
+
+    public String getKey() {
+          return key;
+    }
+
+    public int getData() {
+          return data;
+    }
+    
+}
